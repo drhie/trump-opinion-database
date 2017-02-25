c9ac2525553101fe169849152eb174178ae46cbb
drhie <rhie.david@gmail.com>
2017-02-25 17:11:19 -0500

    Correct CSS for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "test"
+      username == "test" && password == "1234"
     end
   end
 end
